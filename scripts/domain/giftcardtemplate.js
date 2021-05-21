5eaa96129dd86a95817a3cd4e8047998e34db95f
afran012 <afran.012@gmail.com>
2021-05-21 18:33:43 -0500

    add new files

---
+++ scripts/domain/giftcardtemplate.js
@@ -0,0 +1,41 @@
+const gitcardTemplate = (gift) => {
+    let card = document.createElement("div")
+    card.classList.add("div-gifo")  
+
+    let imgGif = document.createElement("img")
+    imgGif.src = urlGif;
+    imgGif.setAttribute("alt", "gif-item");
+    imgGif.classList.add("img-gif")
+    
+
+    let  icons= document.createElement("div")
+    icons.classList.add("div-icons-gifo")
+
+    let imgFav = document.createElement("img")
+    imgFav.src = "./assets/images/icon-fav.svg";
+    imgFav.setAttribute("alt", "icon-fav");
+    imgFav.classList.add("img-fav")
+    
+
+    let imgDown = document.createElement("img")
+    imgDown.src = "./assets/images/icon-download.svg";
+    imgDown.setAttribute("alt", "icon-download");
+    imgDown.classList.add("icon-download")
+    
+
+    let imgFull = document.createElement("img")
+    imgFull.src = "./assets/images/icon-max-normal.svg"
+    imgFull.setAttribute("alt", "icon-max-normal");
+    imgFull.classList.add("icon-max-normal")
+    
+
+
+    card.appendChild(imgGif)  
+    icons.appendChild(imgFav)  
+    icons.appendChild(imgDown)
+    icons.appendChild(imgFull)
+    card.appendChild(icons)
+    //console.log(card)
+    return card
+
+}
